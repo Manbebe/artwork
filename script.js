3e37afb50611d7010021b0f13c798925d8e5145b
Gregory McCormack <gregorym2802@tuckahoeschools.org>
2025-03-10 12:32:11 +0000

    spook dsr

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 //variable for initial sun position
 let sunHeight = 900; //point below horizon
-
+let isVisible = false;
 let horizon = 450
+let timer = 0;
+let duration = 2000
+
 //variables for color change
 let redVal = 0;
 let greenVal = 0;
-let x = random(0, 1600);
-let y = random(550, 900);
+
 
 function setup() {
   createCanvas(1600, 900);
@@ -14,7 +16,7 @@ function setup() {
 function draw() {
   //fill background based on custom variables
   background(redVal, greenVal, 0);
- 
+  
   //sun
   fill(255, 135, 5, 60);
   circle(800, sunHeight, 180);
@@ -41,11 +43,22 @@ function draw() {
   fill("green");
 
   rect(0, horizon, 1600, 500);
-
-  if (mouseIsPressed == true && sunHeight === 130) {
-    background(0);
+  if (isVisible) {
+    ellipse(width / 2, height / 2, 50, 50);
+    if (millis() - timer > duration) {
+      isVisible = false;
+    }
   }
-}
+  if (mouseIsPressed == true && sunHeight === 130) {
+    isVisible = true;
+    timer = millis();
+    let x = random(0, 1600);
+    let y = random(900, 0);
+    fill(0)
+    square(x,y,20)
+
+  }
+};
 
 if (sunHeight > 195) {
   sunHeight -=2;
