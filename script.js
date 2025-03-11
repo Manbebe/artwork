e833be5c3de25c76c26af268e429dccea27a4a28
Gregory McCormack <gregorym2802@tuckahoeschools.org>
2025-03-11 01:03:53 +0000

    i got annoyed and stopped

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 //variable for initial sun position
 let sunHeight = 900; //point below horizon
-let isVisible = false;
 let horizon = 450
-let timer = 0;
-let duration = 2000
-
+let counter = 0
+let help = 255
+let me = 255
 //variables for color change
 let redVal = 0;
 let greenVal = 0;
 
+let img;
+
+// Load the image.
+function preload() {
+  img = loadImage('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/805d3cf6-7d6e-49e8-94eb-7308079bc696/dhue8bn-0146a67c-c555-4778-9338-1446159fd84e.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzgwNWQzY2Y2LTdkNmUtNDllOC05NGViLTczMDgwNzliYzY5NlwvZGh1ZThibi0wMTQ2YTY3Yy1jNTU1LTQ3NzgtOTMzOC0xNDQ2MTU5ZmQ4NGUucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.BkphWVzYtgvSI4HsEzVyi2oI782AGD5DwhrJLqAIcKo');
+}
 
 function setup() {
   createCanvas(1600, 900);
@@ -22,7 +27,7 @@ function draw() {
   circle(800, sunHeight, 180);
   fill(255, 100, 0, 100);
   circle(800, sunHeight, 140);
- 
+
   
  
   // reduce sunHeight by 2 until it reaches 130
@@ -43,22 +48,24 @@ function draw() {
   fill("green");
 
   rect(0, horizon, 1600, 500);
-  if (isVisible) {
-    ellipse(width / 2, height / 2, 50, 50);
-    if (millis() - timer > duration) {
-      isVisible = false;
-    }
-  }
+
   if (mouseIsPressed == true && sunHeight === 130) {
-    isVisible = true;
-    timer = millis();
+    help -=1;
+    me -= 1;
+    counter += 1;
+    background(help, me, 0)
+  }if (counter = 60) {
     let x = random(0, 1600);
     let y = random(900, 0);
-    fill(0)
-    square(x,y,20)
-
+    image(img, x, y, 30,20);
+    counter = 0;
+  } if (mouseIsPressed == false && sunHeight === 130){
+    help = 255;
+    me = 255;
   }
 };
+
+
 
 if (sunHeight > 195) {
   sunHeight -=2;
